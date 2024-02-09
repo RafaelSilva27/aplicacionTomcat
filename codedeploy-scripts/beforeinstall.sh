39d6943e0c143ddf795d11f837aec5dc55603ea5
RafaelSilva27 <radasilva789@gmail.com>
2024-02-09 10:36:24 +0100

    update

--- codedeploy-scripts/beforeinstall.sh
+++ codedeploy-scripts/beforeinstall.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
+mv bundle.tar /tmp
+cd /tmp
+tar -xf bundle.tar
+mkdir primeraApp
+mv -f * /tmp/primeraApp/
+cd /tmp/primeraApp/
 chmod +x gradlew
 bash gradlew war
 mv build/libs/holamundo-0.0.1-SNAPSHOT-plain.war /opt/tomcat/webapps/holamundo-0.0.1-SNAPSHOT-plain.war
