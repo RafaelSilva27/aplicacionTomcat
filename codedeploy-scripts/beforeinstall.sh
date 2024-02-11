d93c9580b43bc6a756dcfdf1881b4b9effcc7ed0
RafaelSilva27 <radasilva789@gmail.com>
2024-02-11 15:02:43 +0100

    cambios con variable id

--- codedeploy-scripts/beforeinstall.sh
+++ codedeploy-scripts/beforeinstall.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
-mv bundle.tar /tmp
-cd /tmp
-tar -xf bundle.tar
-mkdir primeraApp
-mv -f * /tmp/primeraApp/
-cd /tmp/primeraApp/
+# # mv bundle.tar /tmp
+# cd /tmp
+# # tar -xf bundle.tar
+# mkdir primeraApp
+# mv -f * /tmp/primeraApp/
+# cd /tmp/primeraApp/
+# chmod +x gradlew
+# bash gradlew war
+# mv build/libs/holamundo-0.0.1-SNAPSHOT-plain.war /opt/tomcat/webapps/holamundo-0.0.1-SNAPSHOT-plain.war
+
+cd /opt/codedeploy-agent/deployment-root/deployment-group-id/deployment-id/deployment-archive
 chmod +x gradlew
 bash gradlew war
-mv build/libs/holamundo-0.0.1-SNAPSHOT-plain.war /opt/tomcat/webapps/holamundo-0.0.1-SNAPSHOT-plain.war
